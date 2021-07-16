b38e05c22bac687ac34cb3c917508eb669beb971
dm-gthb <indexdm@yandex.ru>
2021-07-16 20:40:23 +0300

    add basic routing

---
+++ src/helpers/renderer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom';
+import Routes from '../client/Routes';
+
+export default (req) => {
+  const content = renderToString(
+    <StaticRouter location={req.path} context={{}}>
+      <Routes />
+    </StaticRouter>
+  );
+
+  return `
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+    </head>
+    <body>
+      <div id="root">${content}</div>
+      <script src="bundle.js"></script> 
+    </body>
+    </html>
+  `;
+};
--- src/index.js
+++ src/index.js
@@ -1,29 +1,13 @@
 import express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import Home from './client/components/Home';
+import renderer from './helpers/renderer';
 
 const PORT = 3002;
 const app = express();
 
 app.use(express.static('public')); 
 
-app.get(`/`, (req, res, next) => {
-  const content = renderToString(<Home/>);
-  const html = `
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-  </head>
-  <body>
-    <div id="root">${content}</div>
-    <script src="bundle.js"></script> 
-  </body>
-  </html>
-`;
-  res.send(html);
-})
+app.get(`* `, (req, res, next) => {
+  res.send(renderer(req));
+});
 
 app.listen(PORT);
