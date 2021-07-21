429db26cd805b1ff041bab7ca380d530607b3f0d
dm-gthb <indexdm@yandex.ru>
2021-07-21 16:49:45 +0300

    add simple auth handling in api service

--- api-service/index.js
+++ api-service/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const { users } = require('./data');
+const cookieParser = require(`cookie-parser`);
 
 const PORT = process.env.API_PORT || 3005;
+const CURRENT_USER = {
+  id: '123',
+  name: 'Current Username'
+};
+
 const app = express();
+
+app.use(cookieParser());
 
 app.use((req, res, next) => {
   res.append('Access-Control-Allow-Origin', ['*']);
@@ -13,6 +21,26 @@ app.use(express.json());
 
 app.get('/users', (req, res) => {
   res.status(200).json(users);
+});
+
+app.get('/currentUser', (req, res) => {
+  if (!req.cookies['is_logged_in']) {
+    res.send(false);
+    return;
+  }
+  
+  res.status(200).json(CURRENT_USER);
+});
+
+app.get('/auth', (req, res, next) => {
+  res.cookie(`is_logged_in`, true, { maxAge: 864000 });
+  res.send(200);
+});
+
+
+app.get('/logout', (req, res, next) => {
+  res.cookie(`is_logged_in`, false, { maxAge: 0 });
+  res.send(200);
 });
 
 app.listen(PORT, () => console.log(`Server starts on ${PORT}`));
